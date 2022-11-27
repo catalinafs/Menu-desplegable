df299a2b17a1168c88fb88af787396122d3b4eeb
catalinafs <114541234+catalinafs@users.noreply.github.com>
2022-11-26 23:50:05 -0500

    delete

--- menu/index.js
+++
@@ -1,9 +0,0 @@
-const menu = document.getElementById('menu')
-
-const menuButton = document.getElementById('openCloseButton')|
-
-document.getElementById('openCloseButton').addEventListener("click", menuExtend)
-
-function menuExtend(evento) {
-    menu.classList.toggle('buttones')
-}
